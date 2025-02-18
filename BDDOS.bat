01c2d2b869093f9c60b000842a6761c530c41005
taha_radi <Abbbas1880@gmail.com>
2025-02-18 19:27:01 +0100

    BDDOS.bat

--- BDDOS.bat
+++ BDDOS.bat
@@ -1,6 +1,19 @@
 @echo off
 title ddos tool made with batch by -taha185
+color 0a
+echo Shout to you :]
+timeout 1
+cls
 set /p ip="Enter target ip: "
 :a
 ping -n 1 -w 1 %ip%
 goto a
+:b
+ping -n 1 -w 1 %ip%
+goto b
+:c
+ping -n 1100-w 1 %ip%
+goto c
+:d
+ping -n 100 -w 1 %ip%
+goto d
